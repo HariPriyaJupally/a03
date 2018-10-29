15a9f8aa12f01a0241b8e26c7fec42acd06bf86b
Hari Priya Jupally <jharipriya@outlook.com>
2018-10-28 21:16:25 -0500

    commit

--- public/js/game.js
+++ public/js/game.js
@@ -3,12 +3,17 @@ function show(number1, number2, number3)
     let side1 = parseInt(number1);
     let side2 = parseInt(number2);
     let side3 = parseInt(number3);
-    side1=Number(document.getElementById("side1"));
-    side2=Number(document.getElementById("side2"));
-    side3=Number(document.getElementById("side3"));
+    side1=Number(document.getElementById('side1') ? parseInt(document.getElementById('side1').value):side1);
+    side2=Number(document.getElementById('side2') ? parseInt(document.getElementById('side2').value):side2);
+    side3=Number(document.getElementById('side3') ? parseInt(document.getElementById('side3').value):side3);
     
-    var perimeter = (side1 + side2 + side3)/2;
-    var area =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3)));
-    alert(area);
-    return area;
+    //var perimeter = (side1 + side2 + side3)/2;
+    
+    //var area =  Math.sqrt(((side1 + side2 + side3)/2)*((((side1 + side2 + side3)/2)-side1)*(((side1 + side2 + side3)/2)-side2)*(((side1 + side2 + side3)/2)-side3)));
+    if(document.getElementById('result'))
+    {
+        document.getElementById('result').value = Math.sqrt(((side1 + side2 + side3)/2)*((((side1 + side2 + side3)/2)-side1)*(((side1 + side2 + side3)/2)-side2)*(((side1 + side2 + side3)/2)-side3)));
+    }
+    //alert(Math.sqrt(((side1 + side2 + side3)/2)*((((side1 + side2 + side3)/2)-side1)*(((side1 + side2 + side3)/2)-side2)*(((side1 + side2 + side3)/2)-side3))));
+    return Math.sqrt(((side1 + side2 + side3)/2)*((((side1 + side2 + side3)/2)-side1)*(((side1 + side2 + side3)/2)-side2)*(((side1 + side2 + side3)/2)-side3)));
 }
--- public/test/test.js
+++ public/test/test.js
@@ -1,11 +1,11 @@
 QUnit.test('Testing the new show function with three sets of inputs', function (assert) {
     assert.equal(show(1, 2, 3), 0, "cannot form a triangle");
-    assert.equal(show(2.3, 2.3, 2.3), 2.29063719300984, "works with three positive real numbers");
+    assert.equal(show(2.3, 2.3, 2.3), 1.7320508075688772, "works with three positive real numbers");
     
 });
 
 QUnit.test('Testing the new show function with four sets of inputs', function (assert) {
-    assert.equal(show(3, 3, 2), 2.8284271247461903, "works with three negative integers");
-    assert.equal(show(2.3, 2.3, 2.3), 0, "works with three positive real numbers");
+    assert.equal(show(-3, -3, -2), 2.8284271247461903, "works with three negative integers");
+    assert.equal(show(1.0, 2.2, 3.0), 0, "works with three positive real numbers but cannot form triangle");
    // assert.throws(function () { show(-1); }, /the given value is less than zero/, 'Passing other than number');
 });
