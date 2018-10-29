d14ab920fa060028b09b71d0ca5e0fe4091499be
Hari Priya Jupally <jharipriya@outlook.com>
2018-10-28 19:56:18 -0500

    commit

--- public/js/game.js
+++ public/js/game.js
@@ -1,12 +1,14 @@
-function show()
+function show(number1, number2, number3)
 {
-    let side1;
-    let side2;
-    let side3;
-    side1=Number(document.getElementById("side1").value);
-    side2=Number(document.getElementById("side2").value);
-    side3=Number(document.getElementById("side3").value);
+    let side1 = parseInt(number1);
+    let side2 = parseInt(number2);
+    let side3 = parseInt(number3);
+    side1=Number(document.getElementById("side1"));
+    side2=Number(document.getElementById("side2"));
+    side3=Number(document.getElementById("side3"));
+    
     var perimeter = (side1 + side2 + side3)/2;
     var area =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3)));
     alert(area);
+    return area;
 }
